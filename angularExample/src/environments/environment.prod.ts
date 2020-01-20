6bf8ddb5f1450a2423eafb5eb2da9b1e2404c5a1
sonalis-vgs <sonalishirgure@gmail.com>
2020-01-20 17:37:01 +0530

    first commit

---
+++ angularExample/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBiUlM91uW_kyuFiUwP0YjVpXrx8kl5Ibc",
+    authDomain: "push-notification-f0353.firebaseapp.com",
+    databaseURL: "https://push-notification-f0353.firebaseio.com",
+    projectId: "push-notification-f0353",
+    storageBucket: "push-notification-f0353.appspot.com",
+    messagingSenderId: "148013096068",
+    appId: "1:148013096068:web:2f5137a386189d8069f6d1",
+    measurementId: "G-599VT6F0HJ"
+  }
+};
